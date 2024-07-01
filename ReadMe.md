1febbb5462455515a8a77d47e1ba235366211dc9
Tom Brockington <tbrockington@silhouettesoftware.com>
2024-07-01 11:01:09 +0100

    save

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+**Setting up the Web-proxy for the Silhouette Web App on your machine**
+
+This web-proxy is used to allow access to the Silhouette America cloud which otherwise is not accessible to the web app directly.
+Here is how you can install it on a machine:
+
+1) Download and install Node.js (https://www.nodejs.org) - make sure all options are set
+
+2) Open a terminal and git clone this repo into a folder called 'web-proxy';
+
+```git clone git@codebasehq.com:silhouettesoftware/silhouette-suite/web-proxy.git```
+
+3) cd into the web-proxy folder ```cd web-proxy```
+
+4) enter ```npm intall```
+
+**Running the web-proxy**
+
+Enter 'npm start' to start the web-proxy
+
+    Note: if the terminal window is closed, you will need to start npm to run the web-proxy again
---
+++ src/server.js
@@ -0,0 +1,71 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+// Path
+import { join } from 'path';
+import * as url from 'url';
+// Env
+import { HTTP_URL, PORT } from './utils/config.js';
+
+const app = express();
+app.disable('x-powered-by');
+
+// Add middleware
+app.use(
+  cors({ 
+    origin: "*"
+   // origin: "https://ambitious-ocean-09f9fbd03-preview.westeurope.5.azurestaticapps.net/"
+  })
+);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Create path to HTML
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+// Server interface page
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: join(__dirname, 'views'),
+  });
+});
+
+app.get('/test-get', (req, res) => {
+  res.status(200).send("hello get")
+});
+
+app.post('/test-post', (req, res) => {
+  res.status(201).send("hello post")
+});
+
+// For all unknown requests 404 page returns
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
+});
+
+app.use((error, req, res, next) => {
+  console.error(error)
+
+  if (error.code === 'P2025') {
+    return sendDataResponse(res, 404, 'Record does not exist')
+  }
+
+  return sendDataResponse(res, 500)
+})
+
+// Start our API server
+app.listen(PORT, () => {
+  console.log(
+    `\nServer is running on ${HTTP_URL}${PORT} \n This no longer consumes souls\n`
+  );
+});
